f3831ba7c7a47105ac53e293592b46c27a95f2b5
jkhaimi <jesse.haimi@icloud.com>
2023-08-11 09:41:07 +0300

    unicafe aloitettu

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button>good</button>
+    </div>
+  )
+}
+
+export default App
