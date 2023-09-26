4e4c9a509486521991dd59d00740e4299ecad992
jkhaimi <jesse.haimi@icloud.com>
2023-09-26 15:22:19 +0200

    2.19 done

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
-const [data, setData] = useState([]);
-const [search, setSearch] = useState('');
+  useEffect(() => {
+    fetch('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
 
-useEffect(() => {
-  fetch('https://studies.cs.helsinki.fi/restcountries/api/all')
-    .then((response) => response.json())
-    .then((data) => setData(data))
-})
+  const handleShowCountry = (country) => {
+    setSelectedCountry(country);
+  };
 
-const filteredCountries = data.filter((country) => 
-  country.name.common.toLowerCase().includes(search.toLowerCase()))
+  const handleSearchChange = (e) => {
+    // Reset selectedCountry to null when the input changes
+    setSelectedCountry(null);
+    setSearch(e.target.value);
+  };
+
+  let filteredCountries = data.filter((country) =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (selectedCountry) {
+    filteredCountries = [selectedCountry];
+  }
 
   return (
     <div>
@@ -22,34 +36,40 @@ const filteredCountries = data.filter((country) =>
         <input
           placeholder='search for countries...'
           value={search} 
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={handleSearchChange}
         />
       </div>
+      
       {filteredCountries.length > 10 ? (
         <div> Too many matches, specify another filter </div>
       ) : (
-        filteredCountries.length === 1 ? (
-          filteredCountries.map((country) => (
-            <div key={country.name.common}>
-              <h1>{country.name.common}</h1>
-              <p>capital {country.capital}</p>
-              <p>area {country.area}</p>
-              <h3>Languages:</h3>
-              <ul>
-                {Object.entries(country.languages).map(([key, language]) => (
-                  <li key={key}>
-                    {language}
-                  </li>
-                ))}
-              </ul>
-              <img src={country.flags.png}/>
-            </div>
-          ))
-        ) : (
-          filteredCountries.map((country) => (
-            <div key={country.name.common}>{country.name.common}</div>
-          ))
-        )
+        filteredCountries.map((country) => (
+          <div key={country.name.common}>
+            {selectedCountry === country || filteredCountries.length === 1 ? (
+              <div>
+                <h1>{country.name.common}</h1>
+                <p>capital {country.capital}</p>
+                <p>area {country.area}</p>
+                <h3>Languages:</h3>
+                <ul>
+                  {Object.entries(country.languages).map(([key, language]) => (
+                    <li key={key}>
+                      {language}
+                    </li>
+                  ))}
+                </ul>
+                <img src={country.flags.png} alt="flag" />
+              </div>
+            ) : (
+              <div>
+                <span>{country.name.common}</span>
+                <button onClick={() => handleShowCountry(country)}>
+                  show
+                </button>
+              </div>
+            )}
+          </div>
+        ))
       )}
     </div>
   );
